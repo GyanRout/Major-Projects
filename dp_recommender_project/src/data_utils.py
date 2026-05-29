c53d1d0a3fa3b710bad58017e5f126b0d8e1d618
GyanRout <gyanrout224@gmail.com>
2026-05-29 12:34:00 +0530

    makeing the variables to be modifiable

--- dp_recommender_project/src/data_utils.py
+++ dp_recommender_project/src/data_utils.py
@@ -5,15 +5,18 @@ import torch
 
 class Loader(Dataset):
     def __init__(self, data_df: pd.DataFrame):
-        required_cols = {'user_id','movie_id','ratings'}
+        user_col = 'user_idx'
+        movie_col = 'movie_idx'
+        rating_col = 'ratings'
+        required_cols = {user_col,movie_col,rating_col}
         if not required_cols.issubset(data_df.columns):
             raise ValueError(f"Require correct column names")
         if data_df.isnull().values.any():
             raise ValueError(f"DataFrame contains NAN, clean the dataframe beforehand")
         
-        self.user_id=torch.tensor(data_df['user_id'].values, dtype=torch.int64)
-        self.movie_id=torch.tensor(data_df['movie_id'].values, dtype=torch.int64)
-        self.ratings=torch.tensor(data_df['ratings'].values, dtype=torch.float32)
+        self.user_id=torch.tensor(data_df[user_col].values, dtype=torch.int64)
+        self.movie_id=torch.tensor(data_df[movie_col].values, dtype=torch.int64)
+        self.ratings=torch.tensor(data_df[rating_col].values, dtype=torch.float32)
 
     def __len__(self)-> int:
         return len(self.user_id)
